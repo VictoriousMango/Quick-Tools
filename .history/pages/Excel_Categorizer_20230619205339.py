0db1828efe6c1e1f7626a58786eadf48edfae713
VictoriousMango <additya.mishra20@st.niituniversity.in>
2023-06-19 20:55:43 +0530

    Excel Output

--- .history/pages/Excel_Categorizer_20230619205339.py
+++ .history/pages/Excel_Categorizer_20230619205339.py
@@ -8,7 +8,7 @@ uploaded_files = st.file_uploader("Choose a CSV file", type=["xlsx"])
 df = None
 
 if uploaded_files:
-    df = pd.read_excel(uploaded_files)
+    df = pd.read_excel(uploaded_files, engine="openpyxl")
 
 if df is not None:
     st.dataframe(df)
