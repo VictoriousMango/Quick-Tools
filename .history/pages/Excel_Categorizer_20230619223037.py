2ccf0db243fac826034dc4c4c6f448e97e694572
VictoriousMango <additya.mishra20@st.niituniversity.in>
2023-06-19 22:30:54 +0530

    Removing Requirements

---
+++ .history/pages/Excel_Categorizer_20230619223037.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+import pandas as pd
+
+
+st.header('Excel Categorizer')
+
+df = None
+
+file_path = st.file_uploader("Upload Excel file", type=["xlsx"])
+if file_path:
+    df = pd.read_excel(file_path)
+    
+if df is not None:
+    st.dataframe(df)
