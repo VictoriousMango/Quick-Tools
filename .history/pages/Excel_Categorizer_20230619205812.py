5ada7b06ca2201ad10a4fadb658f9bb96332bd46
VictoriousMango <additya.mishra20@st.niituniversity.in>
2023-06-19 20:58:34 +0530

    Removing requirements.txt

---
+++ .history/pages/Excel_Categorizer_20230619205812.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+
+st.header('Excel Categorizer')
+
+uploaded_files = st.file_uploader("Choose a CSV file", type=["xlsx"])
+
+df = None
+
+if uploaded_files:
+    df = pd.read_excel(uploaded_files)
+
+if df is not None:
+    st.dataframe(df)
