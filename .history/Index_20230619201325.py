2f0f93c49e35452e1812136dcfafdf22dd8f82f9
VictoriousMango <additya.mishra20@st.niituniversity.in>
2023-06-19 20:20:09 +0530

    Adding Initial Page

---
+++ .history/Index_20230619201325.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='Quick-Tools',
+    page_icon='❤️'
+)
+
+st.header('Welcome User')
---
+++ Home Page.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='Quick-Tools',
+    page_icon='❤️'
+)
+
+st.header('Welcome User')
+st.sidebar.success('App Working Perfectly')
