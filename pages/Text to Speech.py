e002f243d31a53ec3c99288c44a98c2b8c5ca9ac
VictoriousMango <additya.mishra20@st.niituniversity.in>
2023-07-10 18:37:25 +0530

    Trying something new

--- pages/Text to Speech.py
+++ pages/Text to Speech.py
@@ -1,6 +1,31 @@
 import streamlit as st
+import pyttsx3
+import tempfile
+import os
 
-st.header('Welcome to Text to Speech Converter')
+def convert_to_audio(text):
+    engine = pyttsx3.init()
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_filename = temp_file.name
+        engine.save_to_file(text, temp_filename)
+        engine.runAndWait()
+    return temp_filename
 
-text = st.text_input('Enter your Text')
-st.write(text)
+def main():
+    st.title("StreamSound - Text to Audio Converter")
+
+    # Input text
+    text = st.text_area("Enter text to convert", height=200)
+
+    # Convert button
+    if st.button("Convert to Audio"):
+        if text:
+            audio_file = convert_to_audio(text)
+            audio_bytes = open(audio_file, "rb").read()
+            st.audio(audio_bytes, format="audio/wav")
+            os.remove(audio_file)
+        else:
+            st.warning("Please enter some text to convert.")
+
+if __name__ == "__main__":
+    main()
